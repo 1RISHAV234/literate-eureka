96532a4391a4d3d9ca13f97ac9a595fa192064de
1RISHAV234 <83695942+1RISHAV234@users.noreply.github.com>
2022-01-17 16:52:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload() {
+    //
+}
+function setup() {
+    canvas = createCanvas(350,350);
+    canvas.position(85,220);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw() {
+    image(video,0,0,350,350);
+    rect(25,315,300,10);
+    rect(25,25,300,10);
+    rect(315,25,10,300);
+    rect(25,25,10,300)
+    circle(30,30,50);
+    circle(30,320,50);
+    circle(320,320,50);
+    circle(320,30,50);
+}
